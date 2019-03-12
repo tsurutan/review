fba309d5925ff87e6f0681742ef8cdcbdf3bdeae
tsurutan <ishaninaritaina623@gmail.com>
2019-03-12 10:18:29 +0900

    write test c

--- test.c
+++ test.c
@@ -1,1 +1,34 @@
 #include <stdio.h>
+
+int main() {
+  for(int i=1; i< 1000; i++) {
+    printf("kitayo %d\n", i);
+    for(int j=i+1; j<1000; j++) {
+    for(int j=i+2; j<1000; j++) {
+    for(int j=i+3; j<1000; j++) {
+    for(int j=i+4; j<1000; j++) {
+    for(int j=i+5; j<1000; j++) {
+    for(int j=i+6; j<1000; j++) {
+    for(int j=i+7; j<1000; j++) {
+    for(int j=i+8; j<1000; j++) {
+    for(int j=i+9; j<1000; j++) {
+
+    }
+
+    }
+
+    }
+
+    }
+
+    }
+
+    }
+
+    }
+
+    }
+
+    }
+  }
+}
